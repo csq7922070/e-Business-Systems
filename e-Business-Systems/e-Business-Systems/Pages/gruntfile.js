7e712503b57c58aad2c1e132c71c515c4fb965c9
csq7922070 <275091651@qq.com>
2015-10-25 21:38:21 +0800

    添加了bower相关文件。

---
+++ e-Business-Systems/e-Business-Systems/Pages/gruntfile.js
@@ -0,0 +1,36 @@
+﻿module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+            main: {
+                files: [
+                  { src: ['src/html/index.html'], dest: 'dist/html/index.html', filter: 'isFile' }
+                ]
+            }
+        },
+
+        less: {
+            development: {
+                files: {
+                    "dist/css/index.css": "src/less/index.less"
+                }
+            }
+        },
+
+        watch: {
+            files: ['src/**'],
+            tasks: ['dist']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('dist', ['copy', 'less']);
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['dist', 'watch']);
+};
